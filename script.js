05ec5aad6ad91139a036554f43306f75b33555b1
Matt Shepardson <lupemls@yahoo.com>
2019-12-19 20:11:19 -0800

    added styles to the page and included symbols into the generator

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let userCharType = prompt('Would you like your password to have lowercase, capital, numbers and/or symbols?');
 let length = prompt('How long would you like your password to be?');
 let password = "";
+let containerEl = document.querySelector("container");
+let buttonEl = document.querySelector("#generate");
 
 // array that will contain all characters that will be used in password generation
 let generatorArray = []; 
@@ -9,7 +11,7 @@ let charTypes = {
     lowercase : ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
     capital : ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
     numbers : [1,2,3,4,5,6,7,8,9,0],
-    // symbols : [ ,!,\",#,\$,%,&,\',(,),*,+,",",-,.,/,:,;,<,=,>,?,@,[,\,],^,"_","\`",{,|,},~];
+    symbols : [' ','!','\"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~']
 }
 
 for(let key in charTypes){
@@ -21,12 +23,14 @@ function addCharTypes(key){
         generatorArray = generatorArray.concat(charTypes[key]);
     }
 }
-function passwordGen(num){
-    for(let i = 0; i < parseInt(num); i++){
+function passwordGen(length){
+    for(let i = 0; i < parseInt(length); i++){
         rand = Math.floor(Math.random()*generatorArray.length);
         password = password + generatorArray[rand];
     }
     console.log(password);
-
+    containerEl.textContent = password;
 }
 passwordGen(length);
+
+// buttonEl.addEventListener("click", passwordGen);
